df780e322b3ef496319c0a859aff564f5f75aa6f
Maritza <maritzaesparza05@gmail.com>
2025-02-24 14:52:23 -0600

    Add an extra test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maritza05/basic-cli
 
 go 1.23.1
+
+require github.com/google/go-cmp v0.7.0 // indirect
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/google/go-cmp/cmp"
+	"math"
 	"testing"
 )
 
@@ -8,6 +10,19 @@ func TestDummy(t *testing.T) {
 
 	result := 1 + 1
 	if result != 2 {
+		t.Errorf("expected '%d' but got '%d'", 2, result)
+	}
+
+	opt := cmp.Comparer(func(x, y float64) bool {
+		delta := math.Abs(x - y)
+		mean := math.Abs(x+y) / 2.0
+		return delta/mean < 0.00001
+	})
+
+	x := []float64{1.0, 1.1, 1.2, math.Pi}
+	y := []float64{1.0, 1.1, 1.2, 3.14159265359} // Accurate enough to Pi
+
+	if !cmp.Equal(x, y, opt) {
 		t.Errorf("expected '%d' but got '%d'", 2, result)
 	}
 }
